904f03d5d5ba4b21994c3c0046ac85b22d60ae12
MujtabaMamoun <44872348+MujtabaMamoun@users.noreply.github.com>
2018-12-25 14:51:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("527255604691992596")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,**server ,Venus best server**,`);
 }, 30)
 })
 
